e7c287f15ce227e9c924b8e1faaa8bd5488caba4
ishrak_imam <ishrak.imam@gmail.com>
2017-11-13 00:07:24 +0600

    images folder added inside public folder

--- src/hooks/file-upload.js
+++ src/hooks/file-upload.js
@@ -21,14 +21,14 @@ const moveFiles = (files, filePath) => {
    * try to make directory creation async.
    */
 
-  const dir = path.resolve(__dirname, '../../public', filePath);
+  const dir = path.resolve(__dirname, '../../public/images', filePath);
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
 
   const promises = files.map((file) => {
     let src = path.resolve(__dirname, '../uploads', file);
-    let dest = path.resolve(__dirname, '../../public', filePath, file);
+    let dest = path.resolve(__dirname, '../../public/images', filePath, file);
     return move(src, dest);
   });
   return Promise.all(promises);
